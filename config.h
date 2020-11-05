4871567025f6de055a7809af210c247226c57a47
JozanLeClerc <bousset.rudy@gmail.com>
2020-11-05 17:40:05 +0100

    Back to basics

--- config.h
+++ config.h
@@ -16,7 +16,9 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  * Remember to remove cwscale if you change font
  */
-char *font = "UbuntuMono Nerd Font:size=13:antialias=true:autohint=true:";
+char *font = "DejaVuSansMono Nerd Font:pixelsize=15:antialias=true:autohint=true:";
+/* char *font = "mononoki Nerd Font:pixelsize=16:antialias=true:autohint=true:"; */
+/* char *font = "UbuntuMono Nerd Font:pixelsize=17:antialias=true:autohint=true:"; */
 int borderpx = 0;
 
 /*
