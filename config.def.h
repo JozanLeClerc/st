3e1c804d26130f6eaf99df60d3e200e5c12cc8d5
joe <rbo@gmx.us>
2025-10-17 22:42:25 +0200

    font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminess Nerd Font:size=15";
+static char *font = "Terminess Nerd Font:size=12";
 /* static char *font = "BlexMono Nerd Font:size=13"; */
 static int borderpx = 0;
 
