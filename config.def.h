5aa9f79ffc3dc5b1699be91dc6e459d3f312b463
rudy.bousset <rudy.bousset@leviia.com>
2026-06-04 17:38:00 +0200

    ok

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@
  */
 /* static char *font = "MonaspiceNe Nerd Font:size=12"; */
 /* static char *font = "Terminess Nerd Font:size=15"; */
-static char *font = "Terminus (TTF):size=15";
+static char *font = "Terminus (TTF):size=13";
 /* static char *font = "BlexMono Nerd Font:size=13"; */
 static int borderpx = 0;
 
